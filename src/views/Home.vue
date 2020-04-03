85a68fbcf8dd9eb23106f519218cd357d8817a12
Krzysztof Zbicinski <zbicin@users.noreply.github.com>
2020-04-03 19:50:35 +0200

    Improve jumbotron a bit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
-    <b-jumbotron>
-      <template v-slot:header>GroceriesApp</template>
+    <b-container>
+      <b-jumbotron>
+        <template v-slot:header>GroceriesApp</template>
 
-      <template v-slot:lead>Buy groceries in your neigbourhood, conveniently and safely.</template>
+        <template v-slot:lead>Buy groceries in your neigbourhood, conveniently and safely.</template>
 
-      <hr class="my-4" />
+        <hr class="my-4" />
 
-      <p>Browse through variety of products in local shops, select convenient pickup time, pay safely online.</p>
+        <p>Browse through variety of products in local shops, select convenient pickup time, pay safely online.</p>
 
-      <b-button variant="primary" to="/order">Start buying</b-button>&nbsp;
-      <b-button variant="success" href="#">Do Something Else</b-button>
-    </b-jumbotron>
+        <b-button variant="success" size="lg" to="/order">Start buying</b-button>&nbsp;
+        <b-button variant="light" size="lg" href="#">Do Something Else</b-button>
+      </b-jumbotron>
+    </b-container>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
@@ -40,12 +42,15 @@ hr {
   color: #fff;
   text-shadow: 0 0 10px #000;
 }
+.jumbotron .btn {
+  text-shadow: none;
+}
 .jumbotron > * {
   position: relative;
   z-index: 1;
 }
 .jumbotron:before {
-  background: url("../assets/cover.jpg") center center no-repeat;
+  background: url("../assets/cover2.jpg") center center no-repeat;
   background-size: cover;
   content: "";
   position: absolute;
@@ -54,6 +59,6 @@ hr {
   left: 0;
   bottom: 0;
   z-index: 0;
-  opacity: 0.4;
+  opacity: 0.6;
 }
 </style>

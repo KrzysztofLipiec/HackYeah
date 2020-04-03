91a3c738466187de025dc39fa8b1c1a625bd2c07
kaczor6418 <andrzej6418@gmail.com>
2020-04-03 20:29:31 +0200

    feat(form): add registration form

---
+++ src/views/RegistrationPanel.vue
@@ -0,0 +1,62 @@
+<template>
+    <b-container fluid class="bv-example-row">
+        <b-row>
+            <b-col md="4" offset-md="4">
+                <div class="registration-panel">
+                    <h1>Login to your shop</h1>
+                    <b-form>
+                        <b-form-group
+                                id="email-label"
+                                label="Email address:"
+                                label-for="email"
+                                description="We'll never share your email with anyone else."
+                        >
+                            <b-form-input
+                                    id="email-input"
+                                    v-model="form.email"
+                                    type="email"
+                                    required
+                                    placeholder="Enter email..."
+                            ></b-form-input>
+                        </b-form-group>
+                        <b-form-group
+                                id="password-label"
+                                label="Password:"
+                                label-for="password-label">
+                            <b-form-input
+                                    id="password-input"
+                                    v-model="form.password"
+                                    type="password"
+                                    required
+                                    placeholder="Enter password"
+                            ></b-form-input>
+                        </b-form-group>
+                        <b-button type="submit" variant="primary" @click="login">Login</b-button>
+                    </b-form>
+                </div>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'RegistrationPanel',
+        data() {
+            return {
+                form: {
+                    email: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            login(): void {
+                console.log('Send user data...');
+            }
+        },
+        components: {
+
+        }
+    }
+</script>

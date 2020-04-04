9d9b309ce40b9216f73e5cd8330c3beb67d240f1
klip <krzysztof.lipiec@hotmail.com>
2020-04-04 21:19:12 +0200

    maps

--- server/routers/MapRouter.ts
+++ server/routers/MapRouter.ts
@@ -9,13 +9,13 @@ export class MapRouter extends AbstractRouterHandler {
 
     constructor() {
         super();
-        this.router.route('/map/:lat/:lng/:count').get(this.getNearestRestaurants.bind(this));
+        this.router.route('/map/:lat/:lng/:count').get(this.getNearestShops.bind(this));
     }
     public getData(): IData {
         return null;
     }
 
-    private getNearestRestaurants(req: Request, res: Response): void {
+    private getNearestShops(req: Request, res: Response): void {
         const lat = parseFloat(req.params['lat']);
         const lng = parseFloat(req.params['lng']);
         const count = parseInt(req.params['count']);
@@ -27,6 +27,7 @@ export class MapRouter extends AbstractRouterHandler {
             delta = Math.random() * (0.02);
             result.push({ location: { lat: lat - delta, lng: lng - delta } })
         }
+
         res.json(result);
     }
 }

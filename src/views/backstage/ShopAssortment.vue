2ed7addb96f9ae7a18801f06dac5f7596572e27f
kaczor6418 <andrzej6418@gmail.com>
2020-04-04 16:32:00 +0200

    style(shopAssortment): some styling

--- src/views/backstage/ShopAssortment.vue
+++ src/views/backstage/ShopAssortment.vue
@@ -46,21 +46,21 @@ import {Measure} from "@/interfaces/Measure";
         <b-list-group>
             <b-list-group-item class="d-flex justify-content-between" variant="secondary">
                 <div class="d-flex align-items-center">
-                    <b class="ml-2">Item name</b>
-                    <b class="ml-5">Item availability</b>
-                    <b class="ml-5">Item price</b>
+                    <b class="ml-1 item-name__title">Item name</b>
+                    <b class="ml-1 item-availability__title">Item availability</b>
+                    <b class="ml-1 item-price__title">Item price</b>
                 </div>
                 <div class="d-flex flex-row-reverse align-content-center">
-                    <b-button class="mr-2" @click="applyChanges" variant="primary">Apply changes</b-button>
-                    <b-button class="mr-2" @click="addNewItem" variant="success">Add item</b-button>
+                    <b-button class="mr-2" @click="applyChanges" variant="primary" size="xs">Apply changes</b-button>
+                    <b-button class="mr-2" @click="addNewItem" variant="success" size="xs">Add item</b-button>
                 </div>
             </b-list-group-item>
             <b-list-group-item class="d-flex justify-content-between" v-for="(item, index) in shopItems">
                 <div class="d-flex align-items-center">
-                    <b class="ml-2">{{item.name}}</b>
-                    <b-form-select class="ml-5" v-model="item.availability" :options="availabilityOptions"></b-form-select>
+                    <b class="ml-1 item-name">{{item.name}}</b>
+                    <b-form-select class="ml-1 item-availability" v-model="item.availability" :options="availabilityOptions"></b-form-select>
                     <b-form-input
-                            class="ml-5"
+                            class="ml-1 item-price"
                             v-model="item.price"
                             type="number"
                             required
@@ -184,7 +184,32 @@ export default class ShopAssortment extends Vue {
     }
 </script>
 <style>
+    b {
+        text-align: left;
+    }
     .icon {
         cursor: pointer;
+    }
+    .item-name__title {
+        width: 150px;
+    }
+    .item-availability__title {
+        width: 100px;
+    }
+    .item-price__title
+    {
+        width: 60px;
+    }
+    .item-name {
+        width: 150px;
+        max-width: 150px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+    .item-availability{
+        width: 100px;
+    }
+    .item-price{
+        width: 60px;
     }
 </style>

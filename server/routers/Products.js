5671aa0c9275457dbc2f2abc00a4485d42821bb0
klip <krzysztof.lipiec@hotmail.com>
2020-04-04 19:18:40 +0200

    add edit and delete products

--- server/routers/Products.js
+++ server/routers/Products.js
@@ -5,8 +5,37 @@ class Products extends AbstractRouterHandler_1.AbstractRouterHandler {
     constructor(products = []) {
         super();
         this.products = products;
-        this.router.route('/products').get(this.getAllProducts.bind(this));
-        this.router.route('/products/:shopName').get(this.getProducts.bind(this));
+        this.router.route('/products')
+            .get(this.getAllProducts.bind(this));
+        this.router.route('/products/:shopName')
+            .get(this.getProducts.bind(this))
+            .post(this.addProduct.bind(this));
+        this.router.route('/products/:shopName/:productId')
+            .get(this.getProduct.bind(this))
+            .put(this.editProduct.bind(this))
+            .delete(this.removeProduct.bind(this));
+    }
+    editProduct(req, res) {
+        let item = this._getProduct(req.params.productId);
+        if (item) {
+            let index = this.products.indexOf(item);
+            this.products[index] = req.body;
+        }
+    }
+    removeProduct(req, res) {
+        let itemIndex = this.products.indexOf(this._getProduct(req.params.id));
+        if (itemIndex >= 0) {
+            this.products.splice(itemIndex, 1);
+        }
+    }
+    addProduct(req, res) {
+        let product = req.body;
+        product.id = this.products.length.toString();
+        product.shopName = req.params.shopName;
+        this.products.push(product);
+    }
+    getProduct(req, res) {
+        res.json(this._getProduct(req.params.productId));
     }
     getProducts(req, res) {
         res.json(this.products.filter((product) => {
@@ -19,6 +48,9 @@ class Products extends AbstractRouterHandler_1.AbstractRouterHandler {
     getData(data) {
         data.products = this.products;
         return data;
+    }
+    _getProduct(id) {
+        return this.products.find(product => id === product.id);
     }
 }
 exports.Products = Products;
--- server/routers/Products.ts
+++ server/routers/Products.ts
@@ -8,8 +8,42 @@ export class Products extends AbstractRouterHandler {
 
     constructor(private products: Array<TShopItem> = []) {
         super();
-        this.router.route('/products').get(this.getAllProducts.bind(this));
-        this.router.route('/products/:shopName').get(this.getProducts.bind(this));
+        this.router.route('/products')
+            .get(this.getAllProducts.bind(this));
+        this.router.route('/products/:shopName')
+            .get(this.getProducts.bind(this))
+            .post(this.addProduct.bind(this));
+        this.router.route('/products/:shopName/:productId')
+            .get(this.getProduct.bind(this))
+            .put(this.editProduct.bind(this))
+            .delete(this.removeProduct.bind(this));
+    }
+
+    private editProduct(req: Request, res: Response): void {
+        let item = this._getProduct(req.params.productId);
+        if (item) {
+            let index = this.products.indexOf(item);
+            this.products[index] = req.body;
+        }
+
+    }
+
+    private removeProduct(req: Request, res: Response): void {
+        let itemIndex = this.products.indexOf(this._getProduct(req.params.id));
+        if (itemIndex >= 0) {
+            this.products.splice(itemIndex, 1);
+        }
+    }
+
+    private addProduct(req: Request, res: Response): void {
+        let product = req.body as TShopItem;
+        product.id = this.products.length.toString();
+        product.shopName = req.params.shopName;
+        this.products.push(product);
+    }
+
+    private getProduct(req: Request, res: Response): void {
+        res.json(this._getProduct(req.params.productId));
     }
 
     private getProducts(req: Request, res: Response): void {
@@ -25,5 +59,9 @@ export class Products extends AbstractRouterHandler {
     public getData(data: IData): IData {
         data.products = this.products;
         return data;
+    }
+
+    private _getProduct(id: string): TShopItem {
+        return this.products.find(product => id === product.id);
     }
 }
